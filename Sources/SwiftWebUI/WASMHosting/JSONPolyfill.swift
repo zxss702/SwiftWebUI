f9db976b674b4ccecfb0f86d8a43579fa2e1c875
Carson Katri <carson.katri@gmail.com>
2020-06-01 16:47:38 -0400

    Initial SwiftWASM support

---
+++ Sources/SwiftWebUI/WASMHosting/JSONPolyfill.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Carson Katri on 5/28/20.
+//
+
+import Foundation
---
+++ Sources/SwiftWebUI/WASMHosting/WASMServe.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Carson Katri on 5/27/20.
+//
+
+import Foundation
